e0cc1cdb8e528134c488ada1c465d45fc3c84648
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2022-12-09 16:01:55 +0100

    form reset after submission

--- starterOnly/script/script.js
+++ starterOnly/script/script.js
@@ -10,15 +10,16 @@ const
     inscriptionForm = document.querySelector('.inscription-form'),
     formData = [...document.querySelectorAll('.form-data')],
     textControls = [...document.querySelectorAll('.text-control')],
+    locationsInput = [...document.querySelectorAll('.locations-input')],
     termsOfUse = document.getElementById('terms-of-use'),
     submissionModal = document.querySelector('.submission-modal'),
     closeSubmissionModal = document.querySelector('.close-submission-modal'),
     formRegex = [
         /.{2,}/, //first name regex
         /.{2,}/, //last name regex
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, //email regex
-        /[\S\s]+[\S]+/, // birthday regex
-        /[0-9]/ //qty regex
+        /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.][a-z]{2,5}$/, //email regex
+        /./, // birthday regex
+        /[0-9]+/ //qty regex
     ],
     formInputs = [...document.querySelectorAll('.form-inputs')],
     finalValidation = []
@@ -28,32 +29,31 @@ let modalOpened = false
 // toggle menu
 navigationToggleBtn.addEventListener('click', () => navigationContainer.classList.toggle('responsive'))
 
-console.log(modalBtn)
-
 // displaying modal event
 modalBtn.forEach(btn => btn.addEventListener('click', displayModal));
 closeModal.addEventListener('click', displayModal)
 
-
 // launch modal form
 function displayModal() {
-    console.log('modal click', modalOpened)
     modalbg.style.display = modalOpened ? "none" : "block"
     submissionModal.style.display = 'none'
     modalOpened = !modalOpened
-    console.log('modal click', modalOpened)
 }
 
+// check form data
 inscriptionForm.addEventListener('submit', e => {
     e.preventDefault()
 
-    //reset the array
+    //reset the arrays
     finalValidation.length = 0
+    locationChecked.length = 0
 
     //check the "text-control" inputs => formData 0 to 4
     textControls.forEach((controller, idx) => {
         formValidation(controller.value.match(formRegex[idx]) ? true : false, idx)
     })
+
+    //check the place => formData 5
 
     //check the terms of use => formData 6
     formValidation(termsOfUse.checked, 6)
@@ -63,17 +63,17 @@ inscriptionForm.addEventListener('submit', e => {
 }
 )
 
-const formValidation = (isCorrect, idx) => {
+function formValidation(isCorrect, idx) {
     // if the field is correctly filled => isCorrect === true
     formData[idx].dataset.errorVisible = !isCorrect
     finalValidation.push(isCorrect)
-    console.log(finalValidation)
 }
 
-const formValidate = () => {
-    console.log('ok') // afficher le message de confirmation
-    //inscriptionForm.submit()
-    submissionModal.style.display='block'
+function formValidate() {
+    //reset the form
+    inscriptionForm.reset()
+
+    submissionModal.style.display = 'block'
 }
 
 
